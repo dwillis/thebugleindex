ae60ba8ff3318b1f227ce1af254ea2f47a630b65
Derek Willis <dwillis@gmail.com>
2012-01-16 18:31:24 -0500

    added models and admin

---
+++ thebugle/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from thebugle.models import Subject, Episode, Mention, Speaker, Segment
+
+class SubjectAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    
+class EpisodeAdmin(admin.ModelAdmin):
+    list_display = ('number', 'sub_episode', 'date', 'title')
+    prepopulated_fields = {'slug': ('title',)}
+
+class SpeakerAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    
+class MentionAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'segment', 'speaker', 'timestamp')
+
+class SegmentAdmin(admin.ModelAdmin):
+    list_display = ('episode', 'title')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Episode, EpisodeAdmin)
+admin.site.register(Mention, MentionAdmin)
+admin.site.register(Speaker, SpeakerAdmin)
+admin.site.register(Segment, SegmentAdmin)
---
+++ thebugle/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+
+class Speaker(models.Model):
+    name = models.CharField(max_length=90)
+    slug = models.SlugField(max_length=90)
+    
+    def __unicode__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return '/speakers/%s' % self.slug
+
+class Subject(models.Model):
+    name = models.CharField(max_length=90)
+    slug = models.SlugField(max_length=90)
+    
+    def __unicode__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return '/subjects/%s' % self.slug
+
+class Episode(models.Model):
+    number = models.PositiveIntegerField()
+    sub_episode = models.CharField(max_length=5, null=True, blank=True)
+    date = models.DateField()
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    
+    def __unicode__(self):
+        if self.sub_episode:
+            return "%s(%s) - %s" % (str(self.number), self.sub_episode, self.title)
+        else:
+            return "%s - %s" % (str(self.number), self.title)
+    
+    def get_absolute_url(self):
+        return '/episodes/%s' % self.slug
+    
+class Segment(models.Model):
+    episode = models.ForeignKey(Episode)
+    title = models.CharField(max_length=90)
+    slug = models.SlugField(max_length=90)
+    is_top_story = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return '/segments/%s' % self.slug
+    
+class Mention(models.Model):
+    subject = models.ForeignKey(Subject)
+    segment = models.ForeignKey(Segment)
+    speaker = models.ForeignKey(Speaker)
+    timestamp = models.TimeField()
+    
+    def __unicode__(self):
+        return self.subject
+        
